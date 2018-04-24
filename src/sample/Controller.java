d152fc0107ffc87270bd928e32e5e1c37f10118e
Marek Seďa <marek.seda@gmail.com>
2018-04-24 22:19:55 +0200

    přidán cyklus

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
@@ -7,6 +8,7 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
+import javax.jws.HandlerChain;
 import java.awt.*;
 import java.io.*;
 import java.math.BigDecimal;
@@ -35,7 +37,7 @@ public class Controller {
         String buffer[][];
         buffer = new String[180][];
         FileChooser chooser = new FileChooser();
-        chooser.setInitialDirectory(new File("D:\\Users\\Iklon\\Desktop\\Staz\\SICK"));
+        //chooser.setInitialDirectory(new File("D:\\Users\\Iklon\\Desktop\\Staz\\SICK"));
         File file = chooser.showOpenDialog(null);
         try {
             reader = new FileReader(file);
@@ -52,52 +54,67 @@ public class Controller {
                 buffer[a][b] = sep[b]; //proměná b se počítá od 1 namísto od 0...nevim proč
             }
         }
+        chart.getData().add(series);
         compute(buffer, 5, 1);
     }
 
 
 
     private void compute(String data[][], int cas, int count) {
-        int cyklus=0;
         float vysledek[];
         vysledek = new float[180];
 
-        for (int g=1; g<count+1; g++){
+        for (int i=1; i<count+1; i++){
+            final int g = i;
             //System.out.println("Cyklus g: " + g);
+            AnimationTimer timer = new AnimationTimer() {
+                int i = 0;
+                @Override
+                public void handle(long now) {
+                    i++;
+                    if(i > 40) {
+                        i = 0;
+                        int cyklus = 0;
+                        while(cyklus<180) {
+                            //System.out.println("Cyklus c: " + cyklus);
 
-            while(cyklus<180) {
-                //System.out.println("Cyklus c: " + cyklus);
+                            if(data[cyklus][g] != "") {
+                                if (data[cyklus][g].indexOf("Inf")>=0) {
+                                    //System.out.println("Inf");
+                                    vysledek[cyklus] = 0;
+                                    cyklus++;
+                                }
+                                else {
+                                    if (data[cyklus][g].indexOf("NaN")>=0) {
+                                        //System.out.println("NaN");
+                                        vysledek[cyklus] = 0;
+                                        cyklus++;
+                                    }
+                                    else {
+                                        //System.out.println("Normalni cislo");
+                                        BigDecimal cislo = new BigDecimal(data[cyklus][g]);
+                                        vysledek[cyklus] = cislo.floatValue();
+                                        cyklus++;
+                                    }
+                                }
+                            }
+                        }
+                        draw(vysledek);
 
-                if(data[cyklus][g] != "") {
-                    if (data[cyklus][g].indexOf("Inf")>=0) {
-                        //System.out.println("Inf");
-                        vysledek[cyklus] = 0;
-                        cyklus++;
-                    }
-                    else {
-                        if (data[cyklus][g].indexOf("NaN")>=0) {
-                            //System.out.println("NaN");
-                            vysledek[cyklus] = 0;
-                            cyklus++;
-                        }
-                        else {
-                            //System.out.println("Normalni cislo");
-                            BigDecimal cislo = new BigDecimal(data[cyklus][g]);
-                            vysledek[cyklus] = cislo.floatValue();
-                            cyklus++;
-                        }
                     }
                 }
-            }
-            draw(vysledek);
+            };
+            timer.start();
+
+
         }
     }
 
     private void draw(float data[]) {
-        series.getData().removeAll();
+        series.getData().clear();
         for (int a=0; a<180; a++) {
             series.getData().add(new XYChart.Data(-cos(Math.toRadians(a))*data[a],sin(Math.toRadians(a))*data[a]));
         }
-        chart.getData().add(series);
+
     }
 }
