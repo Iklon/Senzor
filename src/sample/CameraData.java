322ee8af48851ceb962fd4bcca71c3d8ddcf5007
Marek Seďa <marek.seda@gmail.com>
2018-04-25 14:27:40 +0200

    kód

--- src/sample/CameraData.java
+++ src/sample/CameraData.java
@@ -10,47 +10,45 @@ import static java.lang.StrictMath.cos;
 import static java.lang.StrictMath.sin;
 
 public class CameraData {
-    private List<Float> numbers;
-    private Integer from = 0; //ve výchozím nastavení od 0 do 180
-    private Integer to = 180;
+    private List<List<Float>> numbers;
 
     public CameraData(String rawText) {
         numbers = new ArrayList<>();
-        String[] textNumbers = rawText.split("( )+");//není potřeba první nahradit mezery ";" a poté rozsekat. Zjistil jsem, že je možné rozsekat rovnou podle těch mezer
-        for (int i = 0; i < textNumbers.length; i++) { //projdu všechny ty Stringy a převedu na čísla
-            if (textNumbers[i].equals("Inf")) { //odchytím Inf a NaN -> místo nich dám 0. Ostatní převedu na floaty.
-                numbers.add(0f);
-            } else if (textNumbers[i].equals("NaN")) {
-                numbers.add(0f);
-            } else {
-                numbers.add(new BigDecimal(textNumbers[i]).floatValue());
+        String[] rows = rawText.split(";");
+        for (int i = 0; i < rows.length; i++) {
+            rows[i] = rows[i].replaceFirst("\\   ", "");
+        }
+        Integer length = rows[0].split("( )+").length;
+        for (int i = 0; i < length; i++) {
+            numbers.add(new ArrayList<>());
+        }
+        for (int i = 0; i < rows.length; i++) {
+            String[] data = rows[i].split("( )+");
+            for (int j = 0; j < length; j++) {
+                numbers.get(j).add(getNumber(data[j]));
+                System.out.println("I:" + i + " J:" + j);
             }
         }
+        //numbers[i] numbers.get(i); numbers.add(prvek)
     }
 
-    public List<XYChart.Data> getXYChartData(int time) {
+    public List<XYChart.Data> getXYChartData(int set) {
         List<XYChart.Data> data = new ArrayList<>();
-        for (int i = from; i < to; i++) {
-            double x = -cos(Math.toRadians(i)) * (numbers.get(time * 180 + i));
-            double y = sin(Math.toRadians(i)) * (numbers.get(time * 180 + i));
+        for (int i = 0; i < 180; i++) {
+            double x = -cos(Math.toRadians(i)) * (numbers.get(set).get(i));
+            double y = sin(Math.toRadians(i)) * (numbers.get(set).get(i));
             data.add(new XYChart.Data(x, y));
         }
         return data;
     }
 
-    public Integer getFrom() {
-        return from;
-    }
-
-    public void setFrom(Integer from) {
-        this.from = from;
-    }
-
-    public Integer getTo() {
-        return to;
-    }
-
-    public void setTo(Integer to) {
-        this.to = to;
+    private Float getNumber(String number) {
+        if (number.equals("Inf")) { //odchytím Inf a NaN -> místo nich dám 0. Ostatní převedu na floaty.
+            return 50f;
+        } else if (number.equals("NaN")) {
+            return 5f;
+        } else {
+            return new BigDecimal(number).floatValue();
+        }
     }
 }
